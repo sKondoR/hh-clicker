ee0ba03b1daa5522686299a9acf01cc77e12995e
kondrse <kondor85@mail.ru>
2026-02-09 23:20:32 +0300

    fix next config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    serverComponentsExternalPackages: ['playwright'],
+    serverExternalPackages: ['playwright'],
   },
   webpack: (config, { isServer }) => {
     if (!isServer) {

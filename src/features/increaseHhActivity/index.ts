ee7ffd6f62a78917e64b7249ce2770d77fa0429d
kondrse <kondor85@mail.ru>
2026-02-12 00:26:23 +0300

    fix

--- src/features/increaseHhActivity/index.ts
+++ src/features/increaseHhActivity/index.ts
@@ -80,7 +80,7 @@ export class IncreaseHhActivity {
     
     // Нажатие кнопки входа
     await this.page.click('button:has-text("Войти")');
-    await this.page.waitForNavigation({ waitUntil: 'load' });
+    await this.page.waitForURL('https://spb.hh.ru/', { waitUntil: 'load' });
 
     // Проверка успешной авторизации
     const isLoggedIn = await this.checkLoginStatus();

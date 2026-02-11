742dcc93b8417ef3dc58f6984f6b5907f0c00c09
kondrse <kondor85@mail.ru>
2026-02-11 23:52:45 +0300

    revert button colors

--- src/app/ui/HomeForm.tsx
+++ src/app/ui/HomeForm.tsx
@@ -101,7 +101,7 @@ const HomeForm: React.FC = () => {
           <button
             onClick={startScraping}
             disabled={isStartButtonDisabled}
-            className={`flex-1 py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 disabled:opacity-50
+            className={`flex-1 py-2 px-4 border bg-green-500 border-transparent rounded-md shadow-sm text-sm font-medium text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 disabled:opacity-50
               ${isStartButtonDisabled ? '' : ' cursor-pointer hover:bg-green-700'}
             `}
           >
@@ -111,7 +111,7 @@ const HomeForm: React.FC = () => {
           <button
             onClick={stopScraping}
             disabled={isStopButtonDisabled}
-            className={`flex-1 py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 disabled:opacity-50
+            className={`flex-1 py-2 px-4 border bg-red-500 border-transparent rounded-md shadow-sm text-sm font-medium text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 disabled:opacity-50
               ${isStopButtonDisabled ? '' : ' cursor-pointer hover:bg-red-700'}
             `}
           >
